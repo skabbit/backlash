beb1ce04cbbd84008f0e91e36d44941a34338cc2
Alexander Serechenko <skabbit@gmail.com>
2020-08-28 17:01:06 +0300

    video process args

--- video_process.py
+++ video_process.py
@@ -8,22 +8,21 @@ import argparse
 
 from utils import process_image
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--input", dest='str', default="test.mp4")
-# parser.add_argument("--limit", dest='int', default=None)
-# args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", dest='str', default="test.mp4")
+parser.add_argument("--limit", dest='int', default=None)
+args = parser.parse_args()
 
-
-probe = skvideo.io.ffprobe('Russia Riot Police Use Batons at Moscow Protest-A_Zf4EusWJo.mp4')
-
-videogen = skvideo.io.vreader('Russia Riot Police Use Batons at Moscow Protest-A_Zf4EusWJo.mp4')
+probe = skvideo.io.ffprobe(args.input)
+videogen = skvideo.io.vreader(args.input)
 writer = skvideo.io.FFmpegWriter("outputvideo.mp4", outputdict={
     '-vcodec': 'libx264', '-b': '5000000'
 })
 
 total = int(probe['video']['@nb_frames'])
-maximum = 3
+maximum = args.limit if args.limit else total
 current = 0
+
 for frame in tqdm(videogen, total=maximum):
     image = Image.fromarray(frame).convert('RGB')
     result_array = process_image(np.asarray(image))
